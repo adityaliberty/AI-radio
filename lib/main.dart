77ac1348ba45c4b44dc19701acbb4ef0a4d9e62c
adityaliberty <adityaliberty33@gmail.com>
2021-03-02 00:17:51 +0530

    master

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:radio/pages/home_page.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
